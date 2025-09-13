6a9caae76c711db401bb75e3969aba68c88396a3
jhed wharvey mendoza <wrvymm@gmail.com>
2025-09-13 14:02:39 +0800

    ENV

--- express-separation-of-concerns-lab/index.js
+++ express-separation-of-concerns-lab/index.js
@@ -1,11 +1,19 @@
 // index.js
 import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
 import postRoutes from './src/routes/post.routes.js';
 import commentRoutes from './src/routes/comment.routes.js';
 
-const app = express();
-const port = 3000;
 
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 // Mount the post routes
